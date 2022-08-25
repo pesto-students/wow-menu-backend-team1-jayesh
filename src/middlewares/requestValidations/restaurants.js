c23a6b0e655cd8e47c672e7fb376e3aa597244e7
Pranshu-Sharma <sharmapranshu9@gmail.com>
2022-08-25 11:26:24 +0530

    created by added for restaurant

--- src/middlewares/requestValidations/restaurants.js
+++ src/middlewares/requestValidations/restaurants.js
@@ -30,6 +30,7 @@ function getDataSchema(req, res) {
         gstNumber: Joi.string().required(),
         gstPercentage: Joi.number().required(),
         totalTables: Joi.number().required(),
+        createdBy: Joi.string().required()
       });
     }
     case "PATCH": {
