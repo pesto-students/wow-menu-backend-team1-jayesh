a861949bd378fe963df5e43df02e9245b0b416e8
Tanmoy Barash <barash.tanmoy@gmail.com>
2022-08-28 00:20:44 +0530

    Fixed Auth error

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -35,15 +35,15 @@ const authController = {
     res
       .cookie("accessToken", `Bearer ${accessToken}`, {
         httponly: true,
-        // sameSite: "none",
-        // secure: true,
+        sameSite: "none",
+        secure: true,
         maxAge: 1000 * 60 * 30,
         // expires: 1000 * 60 * 30,
       })
       .cookie("refreshToken", `Bearer ${refreshToken}`, {
         httponly: true,
-        // sameSite: "none",
-        // secure: true,
+        sameSite: "none",
+        secure: true,
         maxAge: 1000 * 60 * 60 * 24,
       })
       .header("Access-Control-Allow-Credentials", true)
@@ -58,15 +58,15 @@ const authController = {
     res
       .cookie("accessToken", `Bearer ${accessToken}`, {
         httponly: true,
-        // sameSite: "none",
-        // secure: true,
+        sameSite: "none",
+        secure: true,
         maxAge: 1000 * 60 * 30,
         // expires: 1000 * 60 * 30,
       })
       .cookie("refreshToken", `Bearer ${refreshToken}`, {
         httponly: true,
-        // sameSite: "none",
-        // secure: true,
+        sameSite: "none",
+        secure: true,
         maxAge: 1000 * 60 * 60 * 24,
       })
       .header("Access-Control-Allow-Credentials", true)
@@ -114,9 +114,13 @@ const authController = {
               .status(200)
               .cookie("accessToken", ``, {
                 httponly: true,
+                sameSite: "none",
+                secure: true,
               })
               .cookie("refreshToken", ``, {
                 httponly: true,
+                sameSite: "none",
+                secure: true,
               })
               .json({ message: "Logout successfully" });
           } catch (error) {
